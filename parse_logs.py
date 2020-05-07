0b704b4043793cfbc0f6fd1585199466e4616c9a
MilenaMorozova <50710003+milenamorozova@users.noreply.github.com>
2020-05-07 17:30:02 +0300

    add option count, which write total number of requests to the beginning of the file

--- parse_logs.py
+++ parse_logs.py
@@ -1,7 +1,12 @@
 import re
-import sys
+from optparse import OptionParser
 
-file_name = sys.argv[1]
+
+parser = OptionParser()
+parser.add_option("-c", "--count", action="store_true", dest='counter', help="display the total number of requests")
+options, args = parser.parse_args()
+
+file_name = args[0]
 
 with open(file_name, 'r') as file:
     data = file.read()
@@ -13,11 +18,15 @@ requests = re.split("\[\d*\.\d*ms\]", data)
 result = [[time, request.strip()] for time, request in zip(time_ms, requests)]
 
 with open('unsort_'+file_name, 'w') as file_with_unsorted_logs:
+    if options.counter:
+        file_with_unsorted_logs.write("Total: "+str(len(result))+'\n\n')
     for item in result:
         file_with_unsorted_logs.write("[" + str(item[0]) + "ms] : \n" + item[1] + "\n\n")
 
 sorted_result = sorted(result, reverse=True, key=lambda x: x[0])
 
 with open('sort_'+file_name, 'w') as file_with_sorted_logs:
+    if options.counter:
+        file_with_sorted_logs.write("Total: "+str(len(result))+'\n\n')
     for item in sorted_result:
         file_with_sorted_logs.write("[" + str(item[0]) + "ms] : \n" + item[1] + "\n\n")
